0b63b87da6cedf66fce99fe98b613bcdc8a52f33
luoxingyun9410 <142953293+luoxingyun9410@users.noreply.github.com>
2023-09-19 11:56:20 +1000

    Add files via upload

---
+++ Introduction generator/main.py
@@ -0,0 +1,21 @@
+# input and print
+# print("Hello " + input("Name:") + " You are " + input("Age:"))
+
+# name = "Rachel"
+# print(name + " is 23 years old")
+
+# number1 = input("Please enter the first number:")
+# number2 = input("Please enter the second number:")
+
+# temp = number1
+# number1 = number2
+# number2 = temp
+
+# print(number1)
+# print(number2)
+
+print("Welcome!")
+name = input("Please enter your name:")
+age = input("Please enter your age:")
+job = input("Please enter your job:")
+print("Hi everyone I am "+ name + ", I am " + age + ", I am a " + job)

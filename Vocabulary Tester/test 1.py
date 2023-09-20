25d72ea56516cfd3ddcdd3e3c2a7ad0bed7bd6cc
luoxingyun9410 <142953293+luoxingyun9410@users.noreply.github.com>
2023-09-20 11:32:07 +1000

    test 1

--- Vocabulary Tester/test 1.py
+++ Vocabulary Tester/test 1.py
@@ -1,1 +1,15 @@
+customer_spending = {
+	"White": 2000,
+    "Black": 3000,
+    "Yellow": 12000,
+    "Green": 15000,
+    "Grey": 8000
+}
 
+for i in customer_spending:
+    if customer_spending[i] > 10000:
+        customer_spending[i] == "vip"
+    else:
+        customer_spending[i] == "normal customer"
+
+print(customer_spending)

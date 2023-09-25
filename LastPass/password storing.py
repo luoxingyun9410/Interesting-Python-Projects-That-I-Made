c98ff491df234cc6fe8fae49b6ec5ad4c9240ffe
luoxingyun9410 <stevenluo0514@gmail.com>
2023-09-25 14:45:46 +1000

    Final update

--- LastPass/password storing.py
+++ LastPass/password storing.py
@@ -1,18 +1,32 @@
 import json
 
 def get_password_dic():
-    with open("password.json", "r") as f:
-        return json.loads(f.read())
+        with open("password.json", "r") as f:
+            password_str = f.read()
+            if password_str == "":
+                return {}
+            else:
+                return json.loads(f.read())
+
+def ckeck_name(name):
+    password_dic = get_password_dic()
+    if name in password_dic.keys():
+        return False
+    else:
+        return True
 
 def add_password(name, account, password):
-    password_dic = get_password_dic()
-    password_dic[name] = {
-        "account": account,
-        "password": password
-    }
-    with open ("password.json", "w") as f:
-        f.write(json.dumps(password_dic))
-
+    if ckeck_name(name):
+        password_dic = get_password_dic()
+        password_dic[name] = {
+            "account": account,
+            "password": password
+        }
+        with open ("password.json", "w") as f:
+            f.write(json.dumps(password_dic))
+        return True
+    else:
+        return False
 
 
 print("Welcome!")
@@ -25,6 +39,17 @@ while True:
         name = input("Please enter account name: ")
         account = input("Please enter account: ")
         password = input("Please enter password: ")
-        add_password(name, account, password)
-
+        if add_password(name, account, password):
+            print("Successful!")
+        else:
+            print("It already exists")
+    elif option == "r":
+        name = input("Please enter account name: ")
+        if ckeck_name(name):
+            print("It does not exists")
+        else:
+            password_dic = get_password_dic()
+            account = password_dic["name"]["account"]
+            password = password_dic["name"]["password"]
+            print(f"Account: {account}, Password: {password}")
     

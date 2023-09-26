f822b48a873a88054c58675e246fd2b68344c672
luoxingyun9410 <stevenluo0514@gmail.com>
2023-09-26 10:58:41 +1000

    Update

---
+++ UI BMI calculater/project.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import tkinter.font as tkFont
+
+window = Tk()
+window.title("BMI Calculator")
+window.geometry("300x300")
+window.config(padx=50,pady=50)
+
+height_label = Label(text="Height")
+height_label .grid(row=0,column=0)
+
+height_input=Entry()
+height_input.grid(row=0,column=1)
+
+cm_label = Label(text="cm")
+cm_label.grid(row=0,column=2)
+
+weight_label = Label(text="Weight")
+weight_label.grid(row=1,column=0)
+
+weight_input=Entry()
+weight_input.grid(row=1,column=1)
+
+kg_label = Label(text="kg")
+kg_label.grid(row=1,column=2)
+
+bmi_label = Label(text="Your BMI is ")
+bmi_label.grid(row=2, column=1)
+
+def cal_bmi():
+    height = float(height_input.get())/100
+    weight = float(weight_input.get())
+    bmi = weight / height**2
+    bmi = round(bmi, 1)
+    bmi_label["text"] = f"Your BMI is {bmi}"
+
+button = Button(text="Calculate",command=cal_bmi)
+button.grid(row=3, column=1)
+
+window.mainloop()
--- UI BMI calculater/test 1.py
+++ UI BMI calculater/test 1.py
@@ -1,1 +1,35 @@
-import question as q
+# scores = (20,30,40,50)
+# print(scores[0])
+
+from tkinter import *
+import tkinter.font as tkFont
+
+window = Tk()
+window.title("First project")
+window.geometry("400x400")
+window.maxsize(width="400",height="400")
+window.resizable(True,False)
+
+#label
+my_label = Label(text="How you doing")
+my_label.pack(side="top", pady=20)
+my_label.grid(row=0,column=0)
+my_label["text"] = "Hola"
+
+def click():
+    print(my_entry.get())
+    my_label["text"] = my_entry.get()
+
+#button
+my_button= Button(text="Button", command=click)
+my_button.pack(side="top",pady=10)
+my_button.place(anchor="center", x=200,y=200)
+
+#entry
+my_entry = Entry(width=10)
+my_entry.pack(side="top",pady=15)
+my_entry.place(anchor="center", x=150,y=150)
+my_entry.get()
+# print(tkFont.families())
+
+window.mainloop()
